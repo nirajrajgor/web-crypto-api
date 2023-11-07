bab13ec341b0f941f3eb322cdd05f090de467251
Niraj Rajgor <nirajr1992@gmail.com>
2023-11-06 20:54:28 -0800

    Show encoded, encrypted and decrypted text on UI

--- index.js
+++ index.js
@@ -2,30 +2,39 @@ import keyGeneration from "./keyGeneration.js";
 import deriveKey from "./deriveKey.js";
 
 const { publicKeyJwk, privateKeyJwk } = await keyGeneration();
-
 const derivedKey = await deriveKey(publicKeyJwk, privateKeyJwk);
-console.log("derivedKey--->", derivedKey);
-
 const iv = new TextEncoder().encode("Initialization Vector");
 
-console.log("Text-->", "Hello World");
+document.getElementById("form").addEventListener("submit", handleSubmit);
 
-const encodedText = new TextEncoder().encode("Hello World");
-console.log("encodedText--->", encodedText);
+function handleSubmit(event) {
+  event.preventDefault();
+  const text = event.target.firstElementChild.value;
+  const encodedText = new TextEncoder().encode(text);
+  document.getElementById("encoded-text").innerText = encodedText;
 
-const encryptData = await window.crypto.subtle.encrypt(
-  { name: "AES-GCM", iv },
-  derivedKey,
-  encodedText
-);
-console.log("encryptData--->", encryptData);
+  encrypt(encodedText);
+}
 
-const decryptedData = await window.crypto.subtle.decrypt(
-  { name: "AES-GCM", iv },
-  derivedKey,
-  encryptData
-);
-console.log("decryptedData--->", decryptedData);
+async function encrypt(encodedText) {
+  const encryptData = await window.crypto.subtle.encrypt(
+    { name: "AES-GCM", iv },
+    derivedKey,
+    encodedText
+  );
+  var string = new TextDecoder().decode(encryptData);
+  document.getElementById("encrypted-text").innerText = string;
+  console.log("encryptData--->", encryptData);
+  decrypt(encryptData);
+}
 
-const decodedText = new TextDecoder().decode(decryptedData);
-console.log("decryptedData--->", decodedText);
+async function decrypt(encryptData) {
+  const decryptedData = await window.crypto.subtle.decrypt(
+    { name: "AES-GCM", iv },
+    derivedKey,
+    encryptData
+  );
+
+  const decodedText = new TextDecoder().decode(decryptedData);
+  document.getElementById("decrypted-text").innerText = decodedText;
+}
